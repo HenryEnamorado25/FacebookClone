1afee62b711e2e96f92c3c70c561df48f908a42c
Henry Enamorado <henryenamo@gmail.com>
2021-11-14 13:03:45 -0500

    before materialUi

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello World</h1>
+
+      <Header />
+      {/*APP Body*/}
+      {/*SideBar*/}
+      {/*Feed*/}
+      {/*Widgets*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDeFeALQyuhu_jG9qMHeQ3RhDW1-LzKYL4",
+    authDomain: "facebookclone-37885.firebaseapp.com",
+    projectId: "facebookclone-37885",
+    storageBucket: "facebookclone-37885.appspot.com",
+    messagingSenderId: "981367121767",
+    appId: "1:981367121767:web:98248e193b4f0f86a6f1c2",
+    measurementId: "G-JJF2P0NBHD"
+  };
